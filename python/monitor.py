3aba0ad6f903eefe01ff51fd117691a5467cd064
Brian Erickson <berickson@gmail.com>
2015-09-07 00:33:08 -0700

    survives disconnects and device name changes

--- python/monitor.py
+++ python/monitor.py
@@ -2,11 +2,22 @@
 
 import serial
 import sys
+import datetime
+import time
+import glob
 
-o = sys.stdout
-s = serial.Serial("/dev/ttyACM0")
 while True:
-    if(s.inWaiting() > 0):
-        while(s.inWaiting() > 0):
-            o.write(s.readline())
-        o.flush()
+    for usb_path in glob.glob('/dev/ttyACM*'):
+        try:
+            o = sys.stdout
+            s = serial.Serial(usb_path)
+            while True:
+                if(s.inWaiting() > 0):
+                    while(s.inWaiting() > 0):
+                        l = s.readline()
+                        ts = datetime.datetime.now()
+                        o.write("{0}\t{1}".format(ts,l))
+                        o.flush()
+        except IOError:
+            time.sleep(1.0)
+
