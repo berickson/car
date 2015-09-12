b1977305a758549b9aed7ffafd5a0b343f4b16f9
Brian Erickson <berickson@gmail.com>
2015-09-11 21:09:39 -0700

    added input

--- python/monitor.py
+++ python/monitor.py
@@ -5,6 +5,11 @@ import sys
 import datetime
 import time
 import glob
+from select import select
+
+def kbhit():
+  dr,dw,de = select([sys.stdin],[],[],0)
+  return dr <> []
 
 while True:
     for usb_path in glob.glob('/dev/ttyACM*'):
@@ -12,6 +17,10 @@ while True:
             o = sys.stdout
             s = serial.Serial(usb_path)
             while True:
+                if(kbhit()):
+                   c = raw_input('enter command:')
+                   s.write('{0}\n'.format(c))
+                   
                 if(s.inWaiting() > 0):
                     while(s.inWaiting() > 0):
                         l = s.readline()
