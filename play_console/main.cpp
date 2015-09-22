295394c3855684aa1e3039cd84e96dab5e7601c0
Brian Erickson <berickson@gmail.com>
2015-09-21 18:20:54 -0700

    use standard variable names

--- play_console/main.cpp
+++ play_console/main.cpp
@@ -8,26 +8,26 @@ public:
   double kp = 1.0;
   double ki = 0.0;
   double kd = 0.0;
-  double setpoint = 0.0;
+  double sp = 0.0;
   double sum_error = 0.0;
 
   double min_output = -1.;
   double max_output = 1.;
 
-  double input_value = NAN;
-  double input_time = NAN;
-  double prev_input_value = NAN;
-  double prev_input_time = NAN;
+  double pv = NAN;
+  double t = NAN;
+  double prev_pv = NAN;
+  double prev_t = NAN;
 
-  void set_input(double value, double t) {
-    prev_input_value = input_value;
-    prev_input_time = input_time;
-    input_value = value;
-    input_time = t;
+  void set_pv(double _pv, double _t) {
+    prev_pv = pv;
+    prev_t = t;
+    pv = _pv;
+    t = _t;
 
     // integrate error
-    if(!isnan(prev_input_time)) {
-      sum_error += (input_time-prev_input_time) * (setpoint - input_value);
+    if(!isnan(prev_t)) {
+      sum_error += (t-prev_t) * (sp - pv);
     }
   }
 
@@ -35,19 +35,19 @@ public:
     min_output = _min_output;
     max_output = _max_output;
   }
-  void set_setpoint(double value){
-    setpoint = value;
+  void set_sp(double _sp){
+    sp = _sp;
     sum_error = 0;
   }
 
   double get_output(){
     // use the p term
-    double output = (setpoint - input_value) * kp;
+    double output = (sp - pv) * kp;
 
     // use the d term
-    if(kd != 0.0 && !isnan(prev_input_value) && input_time > prev_input_time){
+    if(kd != 0.0 && !isnan(prev_pv) && t > prev_t){
 
-      double d = ((setpoint  - input_value) - (setpoint - prev_input_value)) / (input_time - prev_input_time);
+      double d = ((sp  - pv) - (sp - prev_pv)) / (t - prev_t);
       output += kd * d;
     }
 
@@ -73,12 +73,12 @@ public:
 
 int main(int argc, char ** argv) {
   Pid pid;
-  pid.set_setpoint(10.);
-  pid.set_input(0,0);
+  pid.set_sp(10.);
+  pid.set_pv(0,0);
   cout << "clamped output: " << pid.get_output() << endl;
   pid.set_min_max_output(-1000,1000);
   cout << "output: " << pid.get_output() << endl;
-  pid.set_input(5,1);
+  pid.set_pv(5,1);
   cout << "output without kd: " << pid.get_output() << endl;
   pid.kd = 1;
   cout << "output with kd: " << pid.get_output() << endl;
