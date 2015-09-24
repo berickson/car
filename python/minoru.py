6d2810df261196cd436ac6fc416004ab5829731e
Brian Erickson <berickson@gmail.com>
2015-09-23 17:33:40 -0700

    works on new minoru

--- python/minoru.py
+++ python/minoru.py
@@ -27,15 +27,16 @@ cams = []
 
 for port in ports:
     cap = cv2.VideoCapture(port)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640/2)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,480/2)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,480)
     cap.set(cv2.cv.CV_CAP_PROP_FPS,1)
     cams.append(cap)
 frame_count=0
+stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,64,31)
 while(cv2.waitKey(1)==-1):
     frame_count += 1
     if frame_count%10 == 0:
-        print(frame_count)
+        print('frames: {0}'.format(frame_count))
     images = []
     for cam in [0,1]:
         cams[cam].grab()
@@ -47,7 +48,6 @@ while(cv2.waitKey(1)==-1):
         images.append(im)
         cv2.imshow('image'+str(cam),im)
     stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,16,15)
-    stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,64,31)
 
     greyL = cv2.cvtColor(images[1], cv2.COLOR_BGR2GRAY)
     greyR = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
@@ -55,8 +55,7 @@ while(cv2.waitKey(1)==-1):
     cv2.imshow('greyR', greyR)
 
     disparity = stereo.compute(greyL,greyR)
-    #cv2.imshow('disparity', disparity)
+    cv2.imshow('disparity', disparity)
     cv2.imshow('equalized disparity', histeq(disparity))
 
-#x=cv2.waitKey(0)
 cv2.destroyAllWindows()
