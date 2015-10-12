236a4f624ebd7d82c5443310dd34d162aa3e31d1
Brian Erickson <berickson@gmail.com>
2015-10-11 21:24:17 -0700

    added logger constants

---
+++ teensy/src/Logger.cpp
@@ -0,0 +1,11 @@
+bool LOG_RPM = false;
+bool LOG_ERROR = true;
+bool LOG_INFO = false;
+bool LOG_TRACE = false;
+bool TRACE_RX = false;
+bool TRACE_PINGS = false;
+bool TRACE_ESC = false;
+bool TRACE_MPU = false;
+bool TRACE_LOOP_SPEED = false;
+bool TRACE_DYNAMICS = false;
+
