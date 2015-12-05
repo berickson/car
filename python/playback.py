7fc5f7ff3beac70881fda1d8c15fac3c02f7e4b9
Brian Erickson <berickson@gmail.com>
2015-12-04 18:43:06 -0800

    started switching to Car.py

--- python/playback.py
+++ python/playback.py
@@ -3,7 +3,7 @@ import datetime
 import time
 import dateutil.parser
 from select import select
-from Car import Car
+from Car import Car, Dynamics
 
 recording_file_path = 'recording.csv'
 recording = open(recording_file_path,'r');
@@ -11,22 +11,24 @@ record_start_time = None
 playback_start_time = datetime.datetime.now()
 
 car = Car()
+dyn = Dynamics()
 car.set_rc_mode()
+
 while True:
   s = recording.readline()
   if not s:
     break
   fields = s.split(',')
-  line_time = dateutil.parser.parse(fields[0])
+  dyn.set_from_log(fields)
   if record_start_time == None:
-    record_start_time = line_time
+    record_start_time = dyn.datetime
   
   t_now = datetime.datetime.now()
-  t_wait = (line_time - record_start_time) - (t_now - playback_start_time)
+  t_wait = (dyn.datetime - record_start_time) - (t_now - playback_start_time)
   if t_wait.total_seconds() > 0:
     time.sleep(t_wait.total_seconds())
     
-  car.set_speed_and_steering(fields[5], fields[3])
+  car.set_speed_and_steering(dyn.esc, dyn.str)
 
 car.set_manual_mode() 
 print 'all done'
