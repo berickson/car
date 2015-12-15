17b6383a3c383298101f51d72b4ffa0040595b77
Brian Erickson <berickson@gmail.com>
2015-12-15 12:14:28 -0800

    better name

--- python/go.py
+++ python/go.py
@@ -2,7 +2,7 @@ import time
 from Car import Car
 car = Car()
 time.sleep(1)
-distance = 800
+distance = 500
 heading = car.dynamics.heading
 #car.set_rc_mode()
 car.forward(distance, goal_heading = heading)
