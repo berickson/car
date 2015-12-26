04946d50a56914688d5fdad14ad5fbd2c5ed18fd
Brian Erickson <berickson@gmail.com>
2015-12-26 10:02:07 -0800

    simplified

--- python/go.py
+++ python/go.py
@@ -1,8 +1,7 @@
-import time
 from Car import Car
 car = Car()
 meters = 1.0
-#car.set_rc_mode()
-car.forward(meters, fixed_steering_us = 1460)
+
+car.forward(meters)
 car.forward(-meters)
-car.set_manual_mode()
+
