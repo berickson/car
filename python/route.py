a7ab63def1961f341ac5b8622285e7afe4692fd4
Brian Erickson <berickson@gmail.com>
2015-12-30 16:15:18 -0800

    progress on cte

---
+++ python/route.py
@@ -0,0 +1,84 @@
+class RouteNode:
+  def __init__(self, secs, x, y, heading_degrees, heading_degrees_adjustment, esc_ms, str_ms, meters_per_second):
+    self.secs = secs
+    self.x = float(x)
+    self.y = float(y)
+    self.heading_degrees = float(heading_degrees)
+    self.heading_degrees_adjustment = float(heading_degrees_adjustment)
+    self.esc_ms = int(esc_ms)
+    self.str_ms = int(str_ms)
+    self.meters_per_second = float(meters_per_second)
+
+class Route:
+  def __init__(self):
+    self.columns = ["secs","x","y","heading","adj","esc","str","m/s"];
+    self.nodes = []
+    # a segment is from node[index] to node[index+1]
+    self.index = 0
+    pass
+  
+  
+  # analyse cross track error at current segment
+  # returns (progress, cte), 
+  #  progress < 0, you haven't reached start
+  #  if progress > 1.0 you have passed the current goal node
+  # 
+  # inspired by Udacity cs373 quiz_6_6.py and quiz_6_7.py
+  def cross_track_error(x,y):
+    # some basic vector calculations
+    dx = nodes[index+1].x - nodes[index].x
+    dy = nodes[index+1].y - nodes[index].y
+    drx = x - nodes[index][0]
+    dry = y - nodes[index][1]
+    
+    # u is the robot estimate projected onto the path segment
+    progress = (drx * dx + dry * dy) / (dx * dx + dy * dy)
+    
+    # the cte is the estimate projected onto the normal of the path segment
+    cte = (dry * dx - drx * dy) / (dx * dx + dy * dy)
+    
+    return (progress, cte)
+  
+  def header_string(self):
+    return ",".join(self.columns)
+    
+  def load_from_file(self, file_path):
+    line_number = 1
+    with open(file_path,'r') as infile:
+      # read first line and make sure it matches expected format
+      if infile.readline().strip() != self.header_string():
+        raise Exception("file doesn't match expected format")
+
+      while True:
+        line_number = line_number + 1
+        l = infile.readline().strip()
+        if l == "":
+          break
+        fields = l.split(',')
+        if len(fields) != len(self.columns):
+          raise Exception("wrong number of columns in data on line {}, expected {} was {}".format(line_number, len(self.columns), len(fields)));
+        self.nodes.append(RouteNode(*fields))
+        
+  def save_to_file(self, file_path):
+    with open(file_path,'w') as f:
+      header = ','.join(self.columns)
+      f.write(header+'\n')
+      for node in self.nodes:
+        line = ",".join(
+          [str(node.secs),
+          str(node.x),
+          str(node.y),
+          str(node.heading_degrees),
+          str(node.heading_degrees_adjustment),
+          str(node.esc_ms),
+          str(node.str_ms),
+          str(node.meters_per_second)]
+          )
+        f.write(line+'\n')
+ 
+ 
+if __name__ == '__main__':
+  route = Route()
+  route.load_from_file('recordings/recording_016.csv.path')
+  route.save_to_file('deleteme.path')
+
