28d51b02da2ba742e1c68360111bd4a820b8db25
Brian Erickson <berickson@gmail.com>
2016-01-08 14:44:25 -0800

    added coordinates for rear of car

--- python/make_route.py
+++ python/make_route.py
@@ -11,7 +11,7 @@ def write_path_from_recording_file(inpath = None, outpath = None, min_length = 0
   
   with open(outpath,'w') as outfile:
     # print header row
-    header = ",".join(["secs","x","y","heading","adj","esc","str","m/s"])
+    header = ",".join(["secs","x","y","rear_x", "rear_y", "reverse", "heading","adj","esc","str","m/s"])
     outfile.write(header+"\n")
 
 #    start_heading = car.heading_degrees()
@@ -19,17 +19,30 @@ def write_path_from_recording_file(inpath = None, outpath = None, min_length = 0
     start = car.dynamics
     current = car.dynamics
     (x,y) = car.front_position()
+    
+    reversing = False
+    
    
     while(car.step()):
       (x_next,y_next) = car.front_position()
+      (rear_x,rear_y) = car.position()
       
-      # skip node if distance threshold hasn't been met
-      if distance(x_next,y_next,x,y) < min_length:
+      #todo: are we missing terminal node when we switched forward / reverse?
+      
+      # see if we are switching direction because we always want to write a node if we are
+      next_reversing = reversing
+      if next.odometer_ticks > current.odometer_ticks:
+        next_reversing = False
+      if next.odometer_ticks < current.odometer_ticks:
+        next_reversing = True
+      
+      # skip node if distance threshold hasn't been met and haven't switched direction
+      if next_reversing == reversing and distance(x_next,y_next,x,y) < min_length:
         continue
-     
       current = next
       (x,y) = (x_next,y_next)
       next = car.dynamics
+      reversing = next_reversing
       
       wheel_ticks = next.odometer_ticks - current.odometer_ticks
       d = wheel_ticks * car.meters_per_odometer_tick
@@ -41,6 +54,9 @@ def write_path_from_recording_file(inpath = None, outpath = None, min_length = 0
          str(seconds),
          str(x), 
          str(y), 
+         str(rear_x),
+         str(rear_y),
+         str(reversing),
  #        str(standardized_degrees(current.heading() - start_heading)),
          str(car.heading_degrees()),
          str(car.heading_adjustment),
