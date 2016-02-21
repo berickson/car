bb5f7243b750e4ddbce4d831814350e66b716538
Brian Erickson <berickson@gmail.com>
2016-02-21 12:02:58 -0800

    start work on drift, untested

--- python/play_route.py
+++ python/play_route.py
@@ -57,6 +57,37 @@ def esc_for_velocity(goal_velocity, current_velocity):
       if goal_velocity == 0.:
         esc_ms = 1500
 
+
+def drift(car):
+  # Start with a highly curved  route
+  route = Route()
+  
+  circles = 3.
+  r = .5 # 1 meter circle
+  theta = 0.
+  while theta < circles*2*pi:
+    route.add_node(r-r*cos(theta),r*sin(theta)
+    
+  play_route(route,car)
+  
+  
+
+  # Have outside front wheel following the desired route (alternatively use front center of car)
+  #Apply 100% speed
+  #Detect drift with these conditions:
+  #Heading change no longer follows Ackerman model
+  #Rear wheels  spinning
+  #While drifting:
+  #Update position as a function of steering angle, heading from imu, delta heading and delta wheel clicks (or velocity) of front wheel.
+  #Using the new car heading, calculate how outside front wheel will need to point to stay on track. Turn to that angle.
+  #If rear wheel is going at unsafe speed, decrease throttle
+  #If the front outside wheel can't turn to the outside enough to stay on the route, decrease throttle to decrease slip. This happens because the rear of the car slipped too far forward compared to the front.
+  #Try to maintain a car angle so that the front wheel still is about 10-20 degrees from its maximum steering angle.
+  #If front wheel can't turn to the outside enough, because it reached maximum steering angle, decrease throttle to create less slip so the rear of the car can swing back.
+  #If car angle is getting too close to path angle, increase throttle.
+  #If getting close to goal, stop drifting
+
+
 def play_route(route, car = None, print_progress = False):
   last_ms = None
   
