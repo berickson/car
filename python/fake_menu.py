57166b33637acd4b9740a18eea845f31cb68fb13
Brian Erickson <berickson@gmail.com>
2016-04-16 11:12:51 -0700

    fake menu can use real lcd

--- python/fake_menu.py
+++ python/fake_menu.py
@@ -4,8 +4,58 @@
 import time
 import curses
 import numpy as np
+import pdb
+from lcd import LCD, Lcd
+#from car import Car
 
+class FakeLcd:
+  def __init__(self,h=2,w=16,x=10,y=10):
+    self.h=h
+    self.w=w
+    self.x=10
+    self.y=10
+    
+    self.stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak() # read keys instantly
+    self.win = curses.newwin(self.h+2,self.w+2,self.y,self.x)
+    self.stdscr.keypad(1)
+    curses.curs_set(0)
+    self.win.addstr(0,0,"hello")
+    self.win.box()
+    self.stdscr.refresh()
+    self.win.refresh()
+    self.stdscr.nodelay(1)
+    
+  # returns a curses key press as an LCD.* constant.  
+  # Returns None if no key pressed.
+  def getch(self):
+    c = self.stdscr.getch()
+    if c == curses.KEY_UP:
+      return LCD.UP
+    if c == curses.KEY_DOWN:
+      return LCD.DOWN
+    if c == curses.KEY_RIGHT:
+      return LCD.RIGHT
+    if c == curses.KEY_LEFT:
+      return LCD.LEFT
+    if c ==  ord(' '):
+      return LCD.SELECT
+    return None
 
+  def __del__(self):
+    curses.nocbreak()
+    self.stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
+    curses.curs_set(1)
+  
+  def display_text(self,s):
+    lines = s.split('\n')
+    for i in range(self.h):
+      if len(lines)<i : break
+      self.win.addstr(i+1,1, lines[i])
+    self.win.refresh()
 
 class Config:
   pass
@@ -90,26 +140,26 @@ class Menu:
     self.items = items
     
   def process_key(self,c):
-    if c == ord('q'):
-      self._quit = True
-    if c == ord(' '):
+#    if c == ord('q'):
+#      self._quit = True
+    if c == LCD.SELECT:
       item =  self.current_item()
       if item.action is not None:
         item.action()
-    if c == curses.KEY_UP:
+    if c == LCD.UP:
       self.position = self.position - 1
       self.position = max(self.position,0)
-    if c == curses.KEY_DOWN:
+    if c == LCD.DOWN:
       self.position = self.position + 1
       self.position = min(self.position,len(self.items)-1)
-    if c == curses.KEY_RIGHT:
+    if c == LCD.RIGHT:
       item =  self.current_item()
       if item.sub_menu is None:
         return
       self.stack.append((self.items,self.position))
       self.items = self.get_item(self.position).sub_menu
       self.position = 0
-    if c == curses.KEY_LEFT:
+    if c == LCD.LEFT:
       if len(self.stack) > 0:
         self.items,self.position = self.stack.pop()
         
@@ -142,36 +192,21 @@ class Menu:
     return self._quit
     
   def show_in_terminal(self):
-    stdscr = curses.initscr()
     try:
-      curses.noecho()
-      curses.cbreak() # read keys instantly
-      win = curses.newwin(self.h+2,self.w+2,self.y,self.x)
-      stdscr.keypad(1)
-      curses.curs_set(0)
-      win.addstr(0,0,"hello")
-      win.box()
-      stdscr.refresh()
-      win.refresh()
-      stdscr.nodelay(1)
-      while not self.quit():
-        win.addstr(1,1,self.text1())
-        win.addstr(2,1,self.text2())
-        win.refresh()
-        c = stdscr.getch()
-        if c!=-1:
+      fake_lcd = FakeLcd()
+      lcd = Lcd()
+      while not False:# self.quit():
+        t = self.text1()+"\n"+self.text2()
+        fake_lcd.display_text(t)
+        lcd.display_text(t)
+        c = lcd.getch()
+        if c is None:
+          c = fake_lcd.getch()
+        if c is not None:
           self.process_key(c)
-        time.sleep(0.01)
-          
+        time.sleep(0.03)
     finally:
-      curses.nocbreak()
-      stdscr.keypad(0)
-      curses.echo()
-      curses.endwin()
-      curses.curs_set(1)
-
-
-
+      del lcd
 
 menu = Menu(main_menu)
 menu.show_in_terminal()
--- python/lcd.py
+++ python/lcd.py
@@ -1,11 +1,33 @@
 import Adafruit_CharLCD as LCD
 
 class Lcd(LCD.Adafruit_CharLCDPlate):
+
   def __init__(self, rows=2, columns=16):
     self.row_count = rows
     self.column_count = columns
     self.current_text = [' '*columns for r in range(rows)]
     super(Lcd, self).__init__()
+    
+
+    self.buttons = {LCD.SELECT, LCD.RIGHT, LCD.DOWN, LCD.UP, LCD.LEFT}
+    self.buttons_pressed = self.read_buttons()
+  
+  def read_buttons(self):
+    old_presses = presses = {}
+    for button in self.buttons:
+      presses[button]=self.is_pressed(button)
+    return presses
+        
+
+  
+  def getch(self):
+    old_presses = self.buttons_pressed
+    self.buttons_pressed = self.read_buttons()
+    for k in self.buttons_pressed:
+      if old_presses[k] == False and self.buttons_pressed[k] == True:
+        return k
+    return None
+    
     
   def display_text(self, s):
    
