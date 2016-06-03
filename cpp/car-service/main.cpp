48f64581c5fbdbd81eaf1bb76853ecf5f409dbd3
Brian Erickson <berickson@gmail.com>
2016-06-02 17:43:29 -0700

    Added split

--- cpp/car-service/main.cpp
+++ cpp/car-service/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 #include <thread>
 #include <unistd.h> // for usleep, etc
 
 using namespace std;
+
+
+vector<string> split(string str, char delimiter=',') {
+  vector<string> internal;
+  stringstream ss(str); // Turn the string into a stream.
+  string tok;
+
+  while(getline(ss, tok, delimiter)) {
+    internal.push_back(tok);
+  }
+
+  return internal;
+}
+
+
 
 class Printer {
 public:
@@ -14,6 +32,13 @@ public:
 
 int main(int, char *[])
 {
+    string s = "TD,939.27,-32,Heading String,5";
+    cout << "splitting string " << s << endl;
+    for(auto i: split(s)){
+        cout << i << endl;
+    }
+
+
     Printer p;
     p.text = "calling a member from a thread";
     thread t1(&Printer::run, p);
