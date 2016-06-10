5b0213059dafbd1d8395884fe6f38787b371b1fd
Brian Erickson <berickson@gmail.com>
2016-06-10 15:21:19 -0700

    Changing track updates route.

--- cpp/car_menu.cpp
+++ cpp/car_menu.cpp
@@ -6,6 +6,18 @@
 #include "filenames.h"
 
 
+struct {
+  string track_name = "desk";
+  string route_name = FileNames().get_route_names(track_name)[0];
+  double max_a = 0.25;
+  double max_v = 1.0;
+  double t_ahead = 0.3;
+  double d_ahead = 0.05;
+  double k_smooth = 0.4;
+  bool capture_video = false;
+} car_settings;
+
+
 void assert0(int n) {
   if(n!=0) {
     throw (string) "expected zero, got" + to_string(n);
@@ -29,21 +41,41 @@ void restart(){
 }
 
 
-string track_name = "desk";
-string get_track_name() {
-  return track_name;
-}
-void set_track_name(string s) {
-  track_name = s;
+SubMenu route_selection_menu{};
+
+
+string get_route_name() {
+  return car_settings.route_name;
 }
 
-string route_name = "";
-string get_route_name() {
-  return route_name;
-}
 void set_route_name(string s) {
-  route_name = s;
+  car_settings.route_name = s;
 }
+
+void update_route_selection_menu() {
+  route_selection_menu.items.clear();
+  vector<string> route_names = FileNames().get_route_names(car_settings.track_name);
+  selection_menu<string>(route_selection_menu, route_names, get_route_name, set_route_name);
+}
+
+
+string get_track_name() {
+  return car_settings.track_name;
+}
+void set_track_name(string s) {
+  car_settings.track_name = s;
+  update_route_selection_menu();
+}
+
+
+
+
+
+SubMenu pi_menu {
+  {"shutdown",shutdown},
+  {"reboot",reboot},
+  {"restart",restart}
+};
 
 
 
@@ -53,26 +85,17 @@ CarMenu::CarMenu() {
 void run_car_menu() {
   FakeCar car;
 
-  SubMenu pi_menu {
-    {"shutdown",shutdown},
-    {"reboot",reboot},
-    {"restart",restart}
-  };
 
 
   SubMenu track_selection_menu{};
   vector<string> track_names = FileNames().get_track_names();
   selection_menu<string>(track_selection_menu, track_names, get_track_name, set_track_name);
 
-  SubMenu route_selection_menu{};
-  vector<string> route_names = FileNames().get_route_names(track_name);
-  selection_menu<string>(route_selection_menu, route_names, get_route_name, set_route_name);
-
-
+  update_route_selection_menu();
 
   SubMenu route_menu {
-    {[](){return (string)"track ["+track_name+"]";},&track_selection_menu},
-    {[](){return (string)"route ["+route_name+"]";},&route_selection_menu}
+    {[](){return (string)"track ["+car_settings.track_name+"]";},&track_selection_menu},
+    {[](){return (string)"route ["+car_settings.route_name+"]";},&route_selection_menu}
 
   };
 
