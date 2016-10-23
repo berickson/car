f7e480f63e0db365623f6ad8fec4f822f6dc01b1
Brian Erickson <berickson@gmail.com>
2016-10-22 23:57:40 -0700

    Using external library from bke-arduino

--- teensy-play/src/teensy-play.cpp
+++ teensy-play/src/teensy-play.cpp
@@ -8,17 +8,15 @@
 
 const uint8_t pin_led = 13;
 Mpu9150 mpu9150;
+
 void setup(){
   Serial.begin(250000);
   delay(1000);
   Wire.begin();
   TRACE_MPU = false;
   delay(10);
-  Serial.println("inside setup");
   pinMode(pin_led, OUTPUT);
-  Serial.println("inside setup2");
   digitalWrite(pin_led,1);
-  Serial.println("inside setup3");
   //delay(10);
   Serial.println("setting orientatino");
   //[0.70, -0.18, -0.67, -0.17]
@@ -28,6 +26,7 @@ void setup(){
   Serial.println("inside setup");
   mpu9150.setup();
   Serial.println("setup complete");
+  mpu9150.enable_interrupts(33);
 
 }
 
@@ -49,6 +48,58 @@ struct LoopTracker {
 
 
 int mpu_reporting_ms = 1000;
+bool calibrating_circle = false;
+double delta_yaw = 0.0;
+double last_yaw = 0.0;
+bool verbose_logging = false;
+
+void print_mpu_status() {
+  Serial.print((String) (millis()/1000) +  " ypr[" + mpu9150.heading() + "," + mpu9150.pitch * 180./PI + "," + mpu9150.roll * 180./PI + "]");
+  Serial.print((String) " a[" + mpu9150.ax+ "," + mpu9150.ay+ ","+ mpu9150.az + "]");
+  if( verbose_logging ) {
+    Serial.print((String) " z[" + mpu9150.zero_adjust.w+ "," + mpu9150.zero_adjust.x+ ","+ mpu9150.zero_adjust.y+ ","+ mpu9150.zero_adjust.z + "]");
+    Serial.print((String) " qr[" + mpu9150.qraw.w+ "," + mpu9150.qraw.x+ ","+ mpu9150.qraw.y+ ","+ mpu9150.qraw.z + "]");
+    Serial.print((String) " ar[" + mpu9150.araw.x+ "," + mpu9150.araw.y+ ","+ mpu9150.araw.z + "]");
+    Serial.print((String) " q[" + mpu9150.q.w+ "," + mpu9150.q.x+ ","+ mpu9150.q.y+ ","+ mpu9150.q.z + "]");
+    Serial.print((String) " g[" + mpu9150.gravity.x+ "," + mpu9150.gravity.y+ ","+ mpu9150.gravity.z + "]");
+  }
+  Serial.println();
+}
+
+void print_help() {
+  Serial.println("? - print help text");
+  Serial.println("g - calibrate based on gravity while held level");
+  Serial.println("r - calibrate drift at rest");
+  Serial.println("t - calibrate front direction while tilting the front up");
+  Serial.println("d - dump (print) calibration constants");
+  Serial.println("m - log readings once a minute");
+  Serial.println("s - log readings once a second");
+  Serial.println("v - toggle verbose logging");
+  Serial.println("l - manually log single reading");
+  Serial.println("c - begin / end circle calibration mode");
+}
+
+void begin_circle_calibration() {
+  calibrating_circle = true;
+  last_yaw = mpu9150.yaw;
+  delta_yaw = 0.0;
+}
+
+void end_circle_calibration() {
+  calibrating_circle = false;
+}
+
+void toggle_verbose_logging() {
+  verbose_logging = !verbose_logging;
+  Serial.print("Verbose logging ");
+  Serial.println( verbose_logging ? "ON" : "OFF");
+}
+
+void dump_calibration_constants() {
+
+}
+
+
 
 void loop() {
   int command = 0;
@@ -56,6 +107,14 @@ void loop() {
     command = Serial.read();
     if(command) {
       switch (command) {
+      case '?':
+        print_help();
+        break;
+
+      case 'd':
+        dump_calibration_constants();
+        break;
+
       case 'g':
         mpu9150.calibrate_as_horizontal();
         break;
@@ -78,25 +137,61 @@ void loop() {
         Serial.println("set to print MPU status on every second");
         break;
 
+      case 'v':
+        toggle_verbose_logging();
+        break;
+
+      case 'l':
+        mpu_reporting_ms = 0;
+        print_mpu_status();
+        break;
+      case 'c':
+        if(calibrating_circle) {
+          end_circle_calibration();
+        } else {
+          begin_circle_calibration();
+        }
+        break;
+
       default:
         break;
       }
     }
   }
+  if(calibrating_circle) {
+    double yaw = mpu9150.yaw;
+    double d = yaw-last_yaw;
+    last_yaw = yaw;
+    if(d < -PI) {
+      d += 2 * PI;
+    } else if (d > PI) {
+       d -= 2 * PI;
+    }
+    int estimated_circles = floor((fabs(delta_yaw) + PI)/(2*PI));
+    if(delta_yaw < 0) {
+      estimated_circles *= -1;
+    }
+    delta_yaw += d;
+    double total_error = delta_yaw - 2 * PI * estimated_circles;
+    double error_per_circle = estimated_circles ==0? 0 : total_error / estimated_circles;
+
+    if( loop_tracker.every_n_ms(1000)) {
+        Serial.println((String) "press 'c' to finalize circle calibration delta_yaw: " + delta_yaw * 180. / PI);
+        Serial.println((String) "estimated circles: " + estimated_circles);
+        Serial.println((String) "Error total: " + total_error * 180. / PI);
+        Serial.println((String) "Error per circle: " + error_per_circle * 108. / PI);
+    }
+  }
+
   loop_tracker.execute();
 
   mpu9150.execute();
-  if(loop_tracker.every_n_ms(mpu_reporting_ms)) {
-    Serial.print((String) (millis()/1000) +  " ypr[" + mpu9150.heading() + "," + mpu9150.pitch * 180./PI + "," + mpu9150.roll * 180./PI + "]");
-    Serial.print((String) " z[" + mpu9150.zero_adjust.w+ "," + mpu9150.zero_adjust.x+ ","+ mpu9150.zero_adjust.y+ ","+ mpu9150.zero_adjust.z + "]");
-    Serial.print((String) " qr[" + mpu9150.qraw.w+ "," + mpu9150.qraw.x+ ","+ mpu9150.qraw.y+ ","+ mpu9150.qraw.z + "]");
-    Serial.print((String) " ar[" + mpu9150.araw.x+ "," + mpu9150.araw.y+ ","+ mpu9150.araw.z + "]");
-    Serial.print((String) " q[" + mpu9150.q.w+ "," + mpu9150.q.x+ ","+ mpu9150.q.y+ ","+ mpu9150.q.z + "]");
-    Serial.print((String) " g[" + mpu9150.gravity.x+ "," + mpu9150.gravity.y+ ","+ mpu9150.gravity.z + "]");
-    Serial.print((String) " a[" + mpu9150.ax+ "," + mpu9150.ay+ ","+ mpu9150.az + "]");
-    Serial.println();
-
+  if(mpu_reporting_ms && loop_tracker.every_n_ms(mpu_reporting_ms)) {
+    Serial.print(loop_tracker.loop_count);
+    Serial.print(" ");
+    print_mpu_status();
   }
-  //
-  digitalWrite(pin_led,micros()%1000000<50000);
+
+  // blink the LED
+  digitalWrite(pin_led,micros()%1000000<5000);
 }
