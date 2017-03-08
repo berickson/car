8879ca1a0a74827d01e71c8da6d9d1fe60316e7b
Brian Erickson <berickson@gmail.com>
2017-03-07 23:34:49 -0800

    Ackerman move wheel seems to work

--- play/python/ackerman.py
+++ play/python/ackerman.py
@@ -13,49 +13,86 @@ theta_dot = (v / L) * tan(delta)
 
 
 '''
+
 import math
+
+class Pose2d:
+    ''' pose of an object in 2d '''
+    def __init__(self, x=0, y=0, theta=0):
+        self.x = float(x)
+        self.y = float(y)
+        self.theta = float(theta)
+
+    def __repr__(self):
+        return '({}, {}, {}°)'.format(self.x, self.y, self.theta * 180/math.pi)
+
+    def rotate_around_point(self, x, y, theta):
+        ''' rotates self around point x,y at angle theta '''
+        dx = self.x - x
+        dy = self.y - y
+        rx = dx * math.cos(theta) - dy * math.sin(theta)
+        ry = dy * math.cos(theta) + dx * math.sin(theta)
+        self.x = rx + x
+        self.y = ry + y
+        self.theta += theta
+
+    def relative_point(self, rx, ry):
+        x = self.x + rx * math.cos(self.theta) - ry * math.sin(self.theta)
+        y = self.y + ry * math.cos(self.theta) + rx * math.sin(self.theta)
+        return (x, y)
+
+    def rotate_around_relative_point(self, rx, ry, theta):
+        x, y = self.relative_point(rx, ry)
+        self.rotate_around_point(x, y, theta)
+
+    def move(self, dx, dy):
+        ''' moves self by offset '''
+        self.x += dx
+        self.y += dy
+
+    def move_relative(self, dx, dy):
+        ''' moves distance relative to pose '''
+        p = Pose2d(dx, dy, 0)
+        p.rotate_around_point(0, 0, self.theta)
+        self.x += p.x
+        self.y += p.y
+
 
 class Ackerman:
     ''' Ackerman Steering'''
-    def __init__(self, x_position=0, y_position=0, theta=0):
-        self.x_position = x_position
-        self.y_position = y_position
-        self.theta = theta
+    def __init__(self, x=0, y=0, theta=0, wheelbase_length=1):
+        self.pose = Pose2d(x, y, theta)
+        self.wheelbase_length = wheelbase_length
+        self.steering_angle = 0
 
-    def wheel_steering_angle(self, wheelbase_length, distance, d_theta):
+    def move_front_wheel(self, wheel_offset_left, distance, d_theta):
         '''move wheel at wheel_x, wheel_y distance ds while car rotates d_theta'''
 
         if d_theta != 0.:
             # First, calculate radius of rotation for the circle that the wheel is on
             r_front = distance / d_theta
 
-            # it's impossilbe to have a turn radius less than the wheelbase length
-            if math.fabs(r_front) < wheelbase_length:
+            # it's impossible to have a turn radius less than the wheelbase length
+            if math.fabs(r_front) < self.wheelbase_length:
                 rho = math.pi/2
             else:
                 # Now find the turn radius of the associated rear wheel
-                r_rear = math.sqrt(r_front**2 - wheelbase_length**2)
+                r_rear = math.sqrt(r_front**2 - self.wheelbase_length**2)
 
                 # Now we can find the steering angle rho
-                rho = math.asin(wheelbase_length/r_rear)
-            if d_theta < 0:
+                rho = math.asin(self.wheelbase_length/r_rear)
+
+#todo: consider negative movement
+            if (d_theta < 0) != (distance < 0):
                 rho = -rho
+                r_rear = - r_rear
+
+            self.steering_angle = rho
+
+            self.pose.rotate_around_relative_point(0, wheel_offset_left+r_rear, d_theta)
+
         else:
             rho = 0.
+            self.pose.move_relative(distance, 0)
+
         return rho
-
-def test():
-    ''' main test '''
-    ackerman = Ackerman()
-    for wheelbase_length in [1., 2., 10.]:
-        for distance in [5.,]:
-            for d_theta_degrees in [-10.]:
-                steering_angle = ackerman.wheel_steering_angle(wheelbase_length, distance, d_theta_degrees * math.pi/180)
-                print('L: {:.2f} distance:{:.2f} d_theta:{:.2f} steering_angle:{:.2f}'.format(
-                    wheelbase_length,
-                    distance,
-                    d_theta_degrees,
-                    steering_angle * 180./math.pi))
-
-if __name__ == '__main__':
-    test()
---
+++ play/python/ackerman_test.py
@@ -0,0 +1,78 @@
+''' unit tests for ackerman module'''
+
+import math
+import unittest
+import ackerman
+
+class PoseTests(unittest.TestCase):
+    ''' unit tests for Pose2d class '''
+    def test_rotate_180(self):
+        '''rotate 180 degrees'''
+        pose = ackerman.Pose2d(5, 7, 3)
+        pose.rotate_around_point(0, 0, math.pi)
+        self.assertAlmostEqual(pose.x, -5)
+        self.assertAlmostEqual(pose.y, -7)
+        self.assertAlmostEqual(pose.theta, 3 + math.pi)
+
+    def test_rotate_90(self):
+        '''rotate 180 degrees'''
+        pose = ackerman.Pose2d(5, 7, 3)
+        pose.rotate_around_point(0, 0, math.pi/2.)
+        self.assertAlmostEqual(pose.x, -7)
+        self.assertAlmostEqual(pose.y, 5)
+        self.assertAlmostEqual(pose.theta, 3 + math.pi/2)
+
+    def test_around_x(self):
+        ''' rotate origin around point '''
+        pose = ackerman.Pose2d(0, 0, 0)
+        pose.rotate_around_point(1, 0, math.pi/2)
+        self.assertAlmostEqual(pose.x, 1)
+        self.assertAlmostEqual(pose.y, -1)
+        self.assertAlmostEqual(pose.theta, math.pi/2)
+        pose.rotate_around_point(1, 0, -math.pi/2)
+        self.assertAlmostEqual(pose.x, 0)
+        self.assertAlmostEqual(pose.y, 0)
+        self.assertAlmostEqual(pose.theta, 0)
+
+
+    def test_move(self):
+        ''' simple movement '''
+        pose = ackerman.Pose2d(1, 1, 0)
+        pose.move(-.5, .25)
+        self.assertAlmostEqual(pose.x, 0.5)
+
+    def test_relative_point(self):
+        pose = ackerman.Pose2d(2, 1, math.pi/2)
+        x, y = pose.relative_point(.25, .5)
+        self.assertAlmostEqual(y, 1.25)
+        self.assertAlmostEqual(x, 1.5)
+
+
+    def test_move_relative(self):
+        pose = ackerman.Pose2d(0, 0, 0)
+        pose.move_relative(.25, -.15)
+        self.assertAlmostEqual(pose.x, 0.25)
+        self.assertAlmostEqual(pose.y, -0.15)
+
+        pose = ackerman.Pose2d(2, 1, math.pi/2)
+        pose.move_relative(.5, .25)
+        self.assertAlmostEqual(pose.x, 1.75)
+        self.assertAlmostEqual(pose.y, 1.5)
+
+
+
+
+class AckermanTests(unittest.TestCase):
+    def test_move_front_wheel(self):
+        car = ackerman.Ackerman(wheelbase_length=1)
+        print(car.pose)
+        car.move_front_wheel(wheel_offset_left=0, distance=0.25, d_theta=2.*math.pi/180)
+        car.move_front_wheel(0, -0.25, -2.*math.pi/180)
+        print(car.pose)
+        self.assertAlmostEqual(car.pose.x, 0)
+        self.assertAlmostEqual(car.pose.y, 0)
+        self.assertAlmostEqual(car.pose.theta, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
