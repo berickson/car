19d29acd8a01a17fadf101495adef2d22d4ce1be
Brian Erickson <berickson@gmail.com>
2017-03-22 21:09:01 -0700

    Added ps to test on car.

--- play/flask/Hello.py
+++ play/flask/Hello.py
@@ -1,24 +1,27 @@
+
 '''
 started from tutorial at https://www.tutorialspoint.com/flask/flask_application.htm
 '''
+import subprocess
 from flask import Flask, request, send_from_directory
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World\n'
+    return subprocess.getoutput("ps")
 
 
 def csv_val(s):
     s = str(s)
 
-    s = s.replace('"','""')
+    s = s.replace('"', '""')
     if "," in s:
         s = '"'+s+'"'
     return s
 
 def csv_line(items):
-    ''' 
+    '''
     returns items foramatted as a line in a csv per rfc4180
     without a trailing carriage return
     '''
@@ -26,25 +29,28 @@ def csv_line(items):
 
 @app.route('/car/status')
 def car_status():
-    try:
-        car_state = {
-            'esc':1500,
-            'str':1700,
-            'us':1234343,
-            'name':'Erickson, Brian',
-            'description': 'he said "it would work".'
-        }
-        all_fields = list(car_state.keys())
-        fields = request.args.get('fields',default=None)
-        if fields is None:
-            fields = all_fields
+    car_state = {
+        'esc':1500,
+        'str':1700,
+        'us':1234343,
+        'name':'Erickson, Brian',
+        'description': 'he said "it would work".'
+    }
+    all_fields = list(car_state.keys())
+    fields = request.args.get('fields', default=None)
+    fields = all_fields if fields is None else fields.split(',')
+    values = []
+    for field in fields:
+        if field in car_state.keys():
+            values.append(car_state[field])
         else:
-            fields = fields.split(',')
-    except:
-        e = sys.exc_info()[0]
-        return(str(e))
-        values = [car_state[field] for field in fields]
-    return csv_line(values)
+            values.append("")
+
+    rv = ""
+    if request.args.get('header', default=False):
+        rv += ",".join(fields)+"\n"
+    rv += csv_line(values)+"\n"
+    return rv
 
 
 @app.route('/<path>')
