4f49cf77044233265f56892d0805fe81c84d9017
Brian Erickson <berickson@gmail.com>
2018-10-07 10:43:13 -0700

    comparison tests

--- cpp/unit-tests/async_buf_tests.cpp
+++ cpp/unit-tests/async_buf_tests.cpp
@@ -24,3 +24,23 @@ TEST(async_buf, performance) {
   std::remove("deleteme.txt");
 
 }
+
+
+TEST(no_async_buf, performance) {
+  auto start = high_resolution_clock::now();
+  const int num_lines = 1000000;
+  duration<float> d1,d2;
+  {
+    //async_buf buf("deleteme.txt");
+    ofstream f("deleteme.txt", ofstream::out);
+    for(int i = 0; i < num_lines; i++) {
+      f << "this is line number" << i << "\n";
+    }
+    d1 = high_resolution_clock::now()-start;
+  }
+  d2 = high_resolution_clock::now()-start;
+  cout << "time to write " << num_lines <<" was " << d1.count() << "seconds" << endl;
+  cout << "time including flush was " << d2.count() << "seconds" << endl;
+  std::remove("deleteme.txt");
+
+}
