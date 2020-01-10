e03ed4be42028ca450fc615656cbb79fa160c8e3
Brian Erickson <berickson@gmail.com>
2020-01-10 15:09:17 -0800

    Added web-server file, doesn't compile yet

---
+++ cpp/web_server.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "string_utils.h"
+#include <map>
+#include <netinet/in.h>
+#include <regex>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <thread>
+#include <unistd.h>
+#include <vector>
+using namespace std;
+
+#include "web_server.h"
+
+Request Request::from_string(string s) {
+  Request r;
+  auto lines = split(s, "\r\n");
+  auto request_line = lines[0];
+  auto request_parts = split(request_line, ' ');
+
+  const static std::regex request_regex(R"(^(\w+)\s+(\S+)\s+(.*)$)");
+  std::smatch matches;
+  std::regex_match(request_line, matches, request_regex);
+  if (matches.size() != 4) {
+    throw string("bad request line");
+  }
+  r.method = matches[1];
+  r.uri = matches[2];
+  r.http_version = matches[3];
+
+  const static std::regex header_regex(R"(^(\S*)\:\s*(\S.*)\s*$)");
+  for (int i = 1; i < lines.size(); ++i) {
+    auto &line = lines[i];
+    regex_match(line, matches, header_regex);
+    if (matches.size() != 3) {
+      break; // end of headers
+    }
+    r.headers[matches[1]] = matches[2];
+  }
+
+  return r;
+}
+
+string Request::to_string() const {
+  stringstream ss;
+  ss << "method: " << method << endl;
+  ss << "uri: " << uri << endl;
+  ss << "http_version: " << http_version << endl;
+  for (auto &it : headers) {
+    ss << it.first << ": " << it.second << endl;
+  }
+  ss << endl;
+  return ss.str();
+}
+
+Response::Response(int fd) {
+  this->fd = fd;
+}
+
+void Response::add_header(string name, string value) {
+  headers[name] = value;
+}
+
+void Response::write_status(int code, string description) {
+  string s = (string) "HTTP/1.1 " + to_string(code) + " " + description + "\r\n";
+  write(s.c_str(), s.size());
+}
+
+void Response::enable_multipart() { multipart = true; }
+
+// writes content-length and bytes
+void Response::write_content(string mime_type, const char *bytes, size_t byte_count) {
+  if (multipart) {
+    if (results_sent == 0) {
+      headers["Content-type"] = "multipart/x-mixed-replace;boundary=--boundarydonotcross";
+      headers["Connection"] = "close";
+      write_headers();
+    }
+    write(multipart_boundary);
+    headers.clear();
+    headers["Content-type"] = mime_type;
+    write_headers();
+    write(bytes, byte_count);
+  } else {
+    headers["Content-type"] = mime_type;
+    headers["Content-length"] = to_string(byte_count);
+    write_headers();
+    write(bytes, byte_count);
+  }
+  ++results_sent;
+}
+
+// marks end of multi-part response
+void Response::end() {
+  if (multipart && fd > 0) {
+    write(multipart_final_boundary);
+    close(fd);
+    this->fd = -1;
+  }
+}
+
+bool Response::is_closed() { return fd <= 0; }
+
+void Response::write_headers() {
+  for (auto header : headers) {
+    string bytes = header.first + ": " + header.second + "\r\n";
+    write(bytes);
+  }
+  write("\r\n");
+}
+
+void Response::write(string s) { write(s.c_str(), s.length()); }
+
+void Response::write(const char *bytes, size_t byte_count) {
+  if (fd > 0) {
+    int i = 0;
+    while (i < byte_count) {
+      int written = send(fd, bytes + i, byte_count - i, MSG_NOSIGNAL);
+      if (written == -1) {
+        throw string("cannot send, client closed");
+      }
+      i += written;
+    }
+  }
+}
+
+typedef std::function<void(const Request &, Response &)> Handler;
+
+void connection_thread(int client_socket_fd, map<string, Handler> *handler_map, Handler *default_handler) {
+  try {
+    // cout << "connected to client socket " << client_socket_fd << endl;
+
+    const size_t buffer_size = 2000;
+    char buffer[buffer_size];
+    // read request
+    fd_set readset;
+    struct timeval timeout;
+
+    while (true) {
+      timeout.tv_sec = 0;
+      timeout.tv_usec = 10000;
+      FD_ZERO(&readset);
+      FD_SET(client_socket_fd, &readset);
+      int result = select(client_socket_fd + 1, &readset, NULL, NULL, &timeout);
+      if (result > 0) {
+        bzero(buffer, buffer_size);
+        auto count_received = recv(client_socket_fd, buffer, buffer_size - 1, MSG_DONTWAIT);
+        buffer[count_received] = 0; // terminate string
+        if (count_received == 0) {
+          // cout << "client disconnected" << endl;
+          close(client_socket_fd);
+          return;
+        }
+        // cout << "------- Request ------------" << endl;
+        // cout << buffer << endl;
+        Request request = Request::from_string(buffer);
+        cout << request.method << " " << request.uri << endl;
+        Response response(client_socket_fd);
+
+        string handler_key = request.method + request.uri;
+        auto it = handler_map->find(handler_key);
+        if (it != handler_map->end()) {
+          it->second(request, response);
+        } else {
+          (*default_handler)(request, response);
+        }
+
+        // cout << "handler complete" << endl;
+        bool closed = response.is_closed();
+        if (closed) {
+          cout << "connection closed" << endl;
+          break;
+        }
+      }
+    }
+    cout << "leaving connection thread" << endl;
+  } catch (string s) {
+    cout << "Error in connection thread: " << s << endl;
+  }
+}
+
+void WebServer::add_handler(string method, string uri, Handler handler) { handler_map[method + uri] = handler; }
+
+void WebServer::run(int port) {
+  int server_socket_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  if (server_socket_fd < 0)
+    throw(string("ERROR opening socket"));
+
+  linger lin;
+  lin.l_onoff = 0;
+  lin.l_linger = 0;
+  setsockopt(server_socket_fd, SOL_SOCKET, SO_LINGER, (const char *)&lin, sizeof(int));
+  int enable = 1;
+  if (setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    throw(string("setsockopt(SO_REUSEADDR) failed"));
+
+  struct sockaddr_in serv_addr;
+  bzero((char *)&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port);
+  if (bind(server_socket_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    throw(string("ERROR on binding"));
+  listen(server_socket_fd, max_connection_count);
+  cout << "web-server listening on port " << port << endl;
+
+  while (true) {
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(server_socket_fd, &readfds);
+    if (select(server_socket_fd + 1, &readfds, NULL, NULL, &tv) > 0) {
+      auto client_socket_fd = accept(server_socket_fd, NULL, NULL);
+      if (client_socket_fd > 0) {
+        // cout << "new client connected" << endl;
+        std::thread t(connection_thread, client_socket_fd, &handler_map, &default_handler);
+        // cout << "waiting for thread" << endl;
+        // TODO: manage threads better, now it runs wild
+        // t.join();
+        pthread_setname_np(t.native_handle(), "web-server-worker");
+        t.detach();
+        // cout << "thread done" << endl;
+      }
+    }
+  }
+}
---
+++ cpp/web_server.h
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <map>
+#include <regex>
+#include <thread>
+#include <vector>
+#include "string_utils.h"
+
+using namespace std;
+
+class Request {
+ public:
+  static Request from_string(string s);
+  string to_string() const;
+  string uri;
+  string method;
+  string http_version;
+
+ private:
+  Request() {}
+  map<string, string> headers;
+};
+
+class Response {
+ public:
+  Response(int fd);
+  void add_header(string name, string value);
+  void write_status(int code = 200, string description = "OK");
+  void enable_multipart();
+  void write_content(string mime_type, const char *bytes, size_t byte_count);
+  void end();
+  bool is_closed();
+
+ private:
+  int results_sent = 0;
+  const string multipart_boundary = "\r\n--boundarydonotcross\r\n";
+  const string multipart_final_boundary = "\r\n--boundarydonotcross--\r\n";
+  bool multipart = false;
+  map<string, string> headers;
+
+  void write_headers();
+  void write(string s);
+  void write(const char *bytes, size_t byte_count);
+
+  int fd;
+};
+
+typedef std::function<void(const Request &, Response &)> Handler;
+
+class WebServer {
+ public:
+  int max_connection_count = 4;
+  void add_handler(string method, string uri, Handler handler);
+  void run(int port);
+
+ private:
+  map<string, Handler> handler_map;
+  Handler default_handler = [](const Request &request, Response &response) {
+    response.write_status(404, "Not found");
+    string message = "Not found";
+    response.write_content("text/plain", message.c_str(), message.size());
+  };
+};
